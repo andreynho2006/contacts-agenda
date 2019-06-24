76722004cbd89abec5f7b0aa901216a1fe6c1969
andreynho2006 <andreynho2006@gmail.com>
2019-06-24 14:52:10 +0300

    create a form to upload a image

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput';
 
 class CreateContact extends Component {
     render() {
         return (
-            <div>Crate Contact</div>
+            <div>
+                <Link
+                className='close-create-contact'
+                to='/'>
+                    Close
+                </Link>
+                <form className='create-contact-form'>
+                    <ImageInput
+                    className='create-contact-avatar-input'
+                    name='avatarURL'
+                    maxHeight={64}
+                    />
+                </form>
+            </div>
         )
     }
 }
