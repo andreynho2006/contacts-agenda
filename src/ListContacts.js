6d6e7637b8d9eb73b1ea562e71c548bee94eb661
andreynho2006 <andreynho2006@gmail.com>
2019-06-24 13:30:54 +0300

    get contacts from API and remove contact from API

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -63,7 +63,7 @@ class ListContacts extends Component {
                             </div>
                             <div className='contact-details'>
                                 <p>{contact.name}</p>
-                                <p>@{contact.handle}</p>
+                                <p>{contact.handle}</p>
                             </div>
                             <button
                                 onClick = { () => onDeleteContact(contact) }
